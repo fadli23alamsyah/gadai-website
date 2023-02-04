f8ff455329af2d5003f520612cb9247bf68cd4f6
unknown <nurfadli231298@gmail.com>
2023-02-05 00:47:40 +0800

    update orm to get customer data in dashboard page

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -58,9 +58,8 @@ class DashboardController extends Controller
 
         $out = Finance::where('status','out')->whereIn('store_id', $stores_id)->sum('total');
 
-        // $customer = Pawn::whereNotIn('id', Release::select('pawn_id')->get())
-        //     ->whereIn('store_id', $stores_id)->count();
-        $customer = Pawn::whereIn('store_id', $stores_id)->count();
+        $customer = Pawn::whereNotIn('id', Release::select('pawn_id')->get())
+            ->whereIn('store_id', $stores_id)->count();
 
         $deadline = Pawn::whereNotIn('pawns.id', Release::select('pawn_id')->get())
             ->join('finances', 'finances.id', '=', 'pawns.finance_id')
